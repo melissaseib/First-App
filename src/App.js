dcc969716d368f71f6843d356c0f3bd9b684ce07
Melissa Seib <melissaseib@Melissas-MacBook-Pro.local>
2018-05-31 09:22:29 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Row, Col, Image, Button, FormControl } from "react-bootstrap";
+import "./style.css";
+import logo from "./logo.svg";
+import pic from "./pic.jpg";
+import { ProfileImage } from "./components"; //alternative to css
+
+class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          height: "100%"
+        }}
+      >
+        <Row>
+          <Col
+            xs={12}
+            md={6}
+            lg={3}
+            style={{
+              paddingLeft: "50px",
+              paddingTop: "50pt",
+              display: "tableCell",
+              verticalAlign: "middle",
+              textAlign: "center"
+            }}
+          >
+            <ProfileImage src={pic} />
+          </Col>
+          <Col
+            xs={12}
+            md={6}
+            lg={3}
+            className="Biotext"
+            //style={{ display: "table-cell";  }}
+            //text-align: center;
+            //color: blue;
+            //margin-left: auto;
+            //margin-right: auto;
+            //display: table-cell;
+            //vertical-align: middle;
+          >
+            <div className="biotextcenter">
+              <h1>Melissa Seib</h1>
+              <p>
+                <br /> mss@quitasls.com
+                <br /> (978)-935-6096
+              </p>
+            </div>
+          </Col>
+          <Col xs={12} md={6} lg={3} className="formText">
+            <h1
+              style={{
+                textAlign: "center",
+                textDecoration: "underline"
+              }}
+            >
+              Contact Me!
+            </h1>
+            <br />
+            <p>Name:</p>
+            <FormControl placeholder="Name" />
+            <br />
+            <p>Email:</p>
+            <FormControl placeholder="Email" />
+            <br />
+            <p>Message:</p>
+            <FormControl placeholder="Message" />
+            <br />
+            <Button onClick={() => this.setState({})} bsStyle="primary" block>
+              {" "}
+              Submit{" "}
+            </Button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Row, Col, Image, Button, FormControl } from "react-bootstrap";
+
+//const, let, var
+export const ProfileImage = styled(Image)`
+  width: 300px;
+  align: center;
+`;
+
+export const InfoInput = styled(FormControl);
